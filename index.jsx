39d58cacea69b051405a2e87902a00bfddf3cce9
johnsavvidis <savvidisjohn1990@yahoo.gr>
2024-01-07 00:30:22 +0200

    Added file and modification of index

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from ".pages/Vans"
 
 function App() {
   return (
@@ -11,12 +12,13 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<h1>Vans page goes here 🚐</h1>} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
