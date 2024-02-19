f051a82fe01ce961fa82488a0ff3589be847dee8
johnsavvidis <savvidisjohn1990@yahoo.gr>
2024-02-19 20:52:03 +0200

    Update Vans.jsx

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 
 export default function Vans() {
+    const [vans, setVans] = React.useState([])
+
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setVans(data.vans))
     }, [])
     
+    console.log(vans)
+
     return (
         <div>
             <h1>Vans page goes here 🚐</h1>
