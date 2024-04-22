bd1f69d3e3f44c37f298c9d124df300c0c7576df
johnsavvidis <savvidisjohn1990@yahoo.gr>
2024-04-22 20:29:43 +0300

    New files

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,14 +1,25 @@
 import React from "react"
 
+/**
+ * {
+    * id: "1", 
+    * name: "Modest Explorer", 
+    * price: 60, 
+    * description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", 
+    * imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
+    * type: "simple"
+ * }
+ */
+
+
 export default function Vans() {
     const [vans, setVans] = React.useState([])
-
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
-    
+
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
             <img src={van.imageUrl} />
@@ -21,8 +32,10 @@ export default function Vans() {
     ))
 
     return (
-        <>
-            {vanElements}
-        </>
+        <div className="van-list-container">
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
     )
 }
